bf37d4d8952dc43bd3942d29b5db762ee13962ef
Wilson Jr <wilson.martins@dextra-sw.com>
2019-05-13 07:21:53 -0300

    Remove unused vars

--- app/src/test/java/com/arctouch/codechallenge/util/DateUtilsTest.kt
+++ app/src/test/java/com/arctouch/codechallenge/util/DateUtilsTest.kt
@@ -1,12 +1,8 @@
 package com.arctouch.codechallenge.util
 
-import com.arctouch.codechallenge.API_KEY
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.util.*
-
-private val EXPECTED_POSTER_URL = "https://image.tmdb.org/t/p/w154/poster?api_key=$API_KEY"
-private val EXPECTED_BACKDROP_URL = "https://image.tmdb.org/t/p/w780/backdrop?api_key=$API_KEY"
 
 class DateUtilsTest {
 
